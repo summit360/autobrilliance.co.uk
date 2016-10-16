5d6db14eeef86671bb536f7ff404bca2b26422dd
Colin Densem <colin@summit360.co.uk>
2016-10-16 17:28:59 +0100

    Initial content build of site

--- Gemfile
+++ Gemfile
@@ -13,3 +13,4 @@ gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
 gem 'middleman-compass', '>= 4.0.0'
 gem 'middleman-deploy', github: 'middleman-contrib/middleman-deploy', branch: 'master'
+gem "middleman-minify-html"
--- config.rb
+++ config.rb
@@ -37,10 +37,17 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  activate :minify_html
+
+  activate :asset_hash
+
+  activate :gzip
+
 end
 
 activate :deploy do |deploy|
